4a83d37f9d3404a8725ab14e0dee5fe71e8e48da
Josh Remaley <joshremaley@gmail.com>
2018-03-05 12:08:43 -0600

    lunchtime

--- bash.js
+++ bash.js
@@ -1,25 +1,11 @@
 // Output a prompt
+var commands = require('./command');
+
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-    switch(data.toString().trim()){
-        case 'pwd':
-        process.stdout.write();
-        process.stdout.write('\nprompt > ');//pwd
-        break;
-        case 'date':
-        var date = new Date();
-        process.stdout.write(date.toString());
-        process.stdout.write('\ndateprompt > ');//print date
-        break;
-
-        default:
-        var cmd = data.toString().trim(); // remove the newline
-        process.stdout.write('You typed: ' + cmd);
-        process.stdout.write('\nprompt > ');
-    }
-
-  
-
+    var args = data.toString().trim().split(' ');
+    //var cmd = data.toString().trim(); // remove the newline
+    commands.command(args);
 });
---
+++ command.js
@@ -0,0 +1,82 @@
+var fs = require('fs');
+var readline = require('readline');
+
+module.exports = {
+
+    pwd: function() {    
+        process.stdout.write(process.env.PWD);
+        process.stdout.write('\nprompt > ');
+    },
+
+    date: function(){
+        var date = new Date();
+        process.stdout.write(date.toString());
+        process.stdout.write('\ndateprompt > ');//print date
+    },
+
+    echo: function(args){
+        let output = args.slice(1).join(' ');
+        process.stdout.write(output);
+        process.stdout.write('\ndateprompt > ');
+    },
+
+    ls: function(){
+
+        fs.readdir('.', function(err, files) {
+            if (err) throw err;
+            files.forEach(function(file) {
+              process.stdout.write(file.toString() + "\n");
+            })
+            process.stdout.write("prompt > ");
+          });
+
+       // process.stdout.write('You typed: ' + cmd);
+        //process.stdout.write('\nprompt > ');
+    },
+
+    cat: function(args){
+        args = args.slice(1);
+        args.forEach(function(ele){
+                fs.readFile('./'+ele,"utf8", (err,data) => {
+                    if(err) {
+                        throw err;
+                    }
+                    process.stdout.write(data)
+                })
+            }
+        )  
+        setTimeout( () => {process.stdout.write('\nprompt > ')},500); //only works if forEach takes less than 500ms!
+    },
+
+    head: function(args){
+        let numlines = 5;
+        let lineArr = [];
+        args = args.slice(1);
+        args.forEach(function(ele){
+                fs.readFile('./'+ele,"utf8", (err,data) => {
+                    if(err) {
+                        throw err;
+                    }
+                    //console.log(data.split('\n'))
+                    lineArr.push(data.toString().split('\n'));
+                    process.stdout.write(lineArr.slice(0,numlines).join(' \n'))
+                })
+            }
+        )
+        setTimeout( () => {process.stdout.write('\nprompt > ')},500); //only works if forEach takes less than 500ms!
+    },
+
+    default: function(args){
+        //console.log(args[0]);
+        process.stdout.write('You typed: ' + args.join(' '));
+        process.stdout.write('\nprompt > ');
+    },
+
+
+
+
+    command: function(args){
+        if(this[args[0]]) this[args[0]](args); //why use brackets and not this.comm
+        else this.default(args);
+    }
+}
